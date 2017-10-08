598dff31f2968892cf115fb7501e7447c995d763
carsin <carsin@users.noreply.github.com>
2017-10-08 11:07:22 -1000

    manjaro: init

---
+++ .screenlayout/3monitor1vert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-5 --off --output DP-4 --primary --mode 1920x1080 --pos 1080x536 --rate 144 --rotate normal --output DP-3 --off --output DP-2 --mode 1920x1080 --rate 144 --pos 3000x536 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --off
