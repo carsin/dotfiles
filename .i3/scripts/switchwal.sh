d2d1e263779178474eaf4dab2d32fc723174fe6e
carsin <carsin@users.noreply.github.com>
2017-11-26 14:59:10 -1000

    move wallpapers

--- .i3/scripts/switchwal.sh
+++ .i3/scripts/switchwal.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-wal -n -i "$HOME/.wallpaper"
+wal -n -i "$HOME/wallpapers"
 nitrogen --head=0 --set-zoom-fill "$(< "${HOME}/.cache/wal/wal")"
 nitrogen --head=1 --set-zoom-fill "$(< "${HOME}/.cache/wal/wal")"
 nitrogen --head=2 --set-zoom-fill "$(< "${HOME}/.cache/wal/wal")"
