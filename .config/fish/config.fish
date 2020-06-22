27f838b0265150263adaa43facb15eac21102a28
carsin <carsin@users.noreply.github.com>
2020-06-21 15:19:48 -1000

    fish: add cargo run alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ alias v="vim"
 alias ls="ls -A --color"
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias tmux="tmux -2"
+alias cr="cargo run"
 
 function fish_greeting
     fortune | cowsay
