9a915488073c4e454f42152ca550cf65a16b623c
carsin <20868514+carsin@users.noreply.github.com>
2020-09-23 11:19:21 -1000

    doom: rust-analyzer -> rls

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -73,3 +73,5 @@
 (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
 (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
 (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+
+(setq rustic-lsp-server 'rls)
