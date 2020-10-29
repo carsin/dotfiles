eae4d311346391e9fa5c08888c8508bd55157b24
carsin <20868514+carsin@users.noreply.github.com>
2020-10-28 14:07:50 -1000

    remove spellcheck

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -71,7 +71,7 @@
        :checkers
        (syntax            ; tasing you for every semicolon you forget
         +childframe)
-       spell              ; tasing you for misspelling mispelling
+       ;; spell              ; tasing you for misspelling mispelling
        ;; grammar            ; tasing grammar mistake every you make
 
        :tools
@@ -93,7 +93,7 @@
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
