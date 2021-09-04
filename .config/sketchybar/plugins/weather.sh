ec1f656c87e12ba80779eec96edead587e5f093e
carsin <20868514+carsin@users.noreply.github.com>
2021-09-04 12:03:46 -1000

    sketchybar cool stuff

---
+++ .config/sketchybar/plugins/weather.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+status=$(curl -s 'wttr.in/96754?format=%C+|+%t')
+
+condition=$(echo $status | awk -F '|' '{print $1}' | tr '[:upper:]' '[:lower:]')
+condition="${condition// /}"
+temp=$(echo $status | awk -F '|' '{print $2}')
+temp="${temp//\+/}"
+temp="${temp// /}"
+
+# add more conditions here as appropriate
+case "${condition}" in
+  "sunny")
+    icon=""
+    ;;
+  "partlycloudy")
+    icon=" "
+    ;;
+  *)
+    icon="$condition"
+    ;;
+esac
+
+sketchybar -m set weather icon "$icon"
+sketchybar -m set weather label "$temp"
