77eda83e960f11845bd4a4e7dcaa4112307f1eea
carsin <20868514+carsin@users.noreply.github.com>
2021-10-14 13:35:20 -0700

    update

--- .config/nvim/lua/plugins/tscope.lua
+++ .config/nvim/lua/plugins/tscope.lua
@@ -48,7 +48,7 @@ telescope.setup {
       },
     },
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = { ".git", "node_modules", "target", ".class" },
+    file_ignore_patterns = { ".git", "node_modules", "target", ".class", ".cache" },
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
