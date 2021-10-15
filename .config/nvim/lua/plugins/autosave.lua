f06a2c69b0f82046c97f5a5619036c2da58e5f4e
carsin <20868514+carsin@users.noreply.github.com>
2021-10-15 10:11:44 -0700

    update

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -4,10 +4,10 @@ autosave.setup({
   enabled = true,
   execution_message = "",
   events = { "InsertLeave", "CursorHoldI", "CursorHold" },
-  write_all_buffers = true,
+  write_all_buffers = false,
   on_off_commands = true,
-  clean_command_line_interval = 1,
-  debounce_delay = 200,
+  clean_command_line_interval = 0,
+  debounce_delay = 500,
   conditions = {
       exists = true,
       filename_is_not = {},
