d60ad47b2d1f6d30c196df6c293c6bf16079371d
carsin <20868514+carsin@users.noreply.github.com>
2021-11-03 10:49:46 -0700

    rlly fix it now

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -76,7 +76,7 @@ return packer.startup({function(use)
     'kosayoda/nvim-lightbulb',
     config = function()
       vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
-      vim.fn.sign_define('LightBulbSign', { text = "?", texthl = "", linehl="", numhl="" })
+      -- vim.fn.sign_define('LightBulbSign', { text = "?", texthl = "", linehl="", numhl="" })
     end
   }
 
