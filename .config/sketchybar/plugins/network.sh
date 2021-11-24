5586686b4ab5fdee1c309e78d9efa78699eadb26
carsin <20868514+carsin@users.noreply.github.com>
2021-11-23 23:35:36 -0800

    network multiadapter montioring v1

--- .config/sketchybar/plugins/network.sh
+++ .config/sketchybar/plugins/network.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-UPDOWN=$(ifstat -i "en6,en0" -b 0.1 1 | tail -n1)
-DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f1 -d ".")
-UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
+UPDOWN_WIFI=$(ifstat -i "en0" -b 0.1 1 | tail -n1)
+export UP=$(echo $UPDOWN_WIFI | awk "{ print \$2 }" | cut -f1 -d ".")
+export DOWN=$(echo $UPDOWN_WIFI | awk "{ print \$1 }" | cut -f1 -d ".")
+echo $DOWN
+
+UPDOWN_ETH=$(ifstat -i "en6" -b 0.1 1 | tail -n1)
+if [[ "$UPDOWN_ETH" != "n/a n/a" ]]; then # ethernet is connected
+  UP_ETH=$(echo $UPDOWN_ETH | awk "{ print \$2 }" | cut -f1 -d ".")
+  DOWN_ETH=$(echo $UPDOWN_ETH | awk "{ print \$1 }" | cut -f1 -d ".")
+  export UP=$(($UP + $UP_ETH))
+  export DOWN=$(($DOWN + $DOWN_ETH))
+fi
 
 
 DOWN_FORMAT=""
@@ -17,9 +26,6 @@ if [ "$UP" -gt "999" ]; then
 else
   UP_FORMAT=$(echo $UP | awk '{ printf "%.0f kbps", $1}')
 fi
-
-echo $UPDOWN
-echo $DOWN_FORMAT
 
 sketchybar -m --set network_up label="$UP_FORMAT" label.highlight=$(if [ "$UP" -gt "0" ]; then echo "on"; else echo "off"; fi)
 sketchybar -m --set network_down label="$DOWN_FORMAT" label.highlight=$(if [ "$DOWN" -gt "0" ]; then echo "on"; else echo "off"; fi)
