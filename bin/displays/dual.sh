48e8e13a9e9ea34311c87f7ecf4543c2b0a62c9d
carsin <20868514+carsin@users.noreply.github.com>
2021-12-16 16:46:26 -1000

    xrandr try duals mixed dpi

--- bin/displays/dual.sh
+++ bin/displays/dual.sh
@@ -1,6 +1,52 @@
 #!/usr/bin/bash
 
 # set up displays
-xrandr --output eDP-1 --off \
-       --output DP-4 --auto --dpi 92 --rate 144.0 --primary \
-       --output DP-5 --auto --dpi 92 --rate 144.0 --right-of DP-4
+# xrandr --output eDP-1 --off \
+#        --output DP-4 --auto --rate 144.0 --primary \
+#        --output DP-5 --auto --rate 144.0 --right-of DP-4
+
+xrandr \
+  --output DP-4 \
+  --primary \
+  --rate 144.0 \
+  --mode 1920x1080 \
+  --fb 7680x5760 \
+  --rotate normal \
+  --scale 1.5x1.5 \
+  --panning 5760x3240+0+0 \
+\
+  --output DP-5 \
+  --rate 144.0 \
+  --rotate normal \
+  --mode 1920x1080 \
+  --right-of DP-4 \
+  --fb 7680x2160 \
+  --scale 1.5x1.5 \
+  --panning 5760x3240+5760+0 \
+\
+  --output eDP-1 --off
+  
+# triple testing
+# xrandr \
+#   --output DP-4 \
+#   --primary \
+#   --mode 3840x2160 \
+#   --fb 10240x5760 \
+#   --rotate normal \
+#   --scale 1.5x1.5 \
+#   --panning 5760x3240+0+0 \
+# \
+#   --output DP-5 \
+#   --rotate normal \
+#   --mode 3840x2160 \
+#   --right-of DP-4 \
+#   --fb 10240x2160   \
+#   --scale 1.5x1.5 \
+#   --panning 5760x3240+5760+0 \
+# \
+#   --output eDP-1 \
+#   --mode 2560x1600
+#   --rotate normal \
+#   --fb 10240x2160   \
+#   --scale 1.5x1.5 \
+#   --panning 5760x3240+5760+0 \
