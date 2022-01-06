49c6ebb931649074d33e52016b5819034e15c41e
carsin <20868514+carsin@users.noreply.github.com>
2022-01-05 22:17:58 -1000

    update

--- bin/displays/ultrawide.sh
+++ bin/displays/ultrawide.sh
@@ -10,14 +10,13 @@ xrandr \
     --primary \
     --dpi $newdpi \
     --mode 3440x1440 \
-    --pos 2560x0 \
     --right-of eDP-1-1 \
     --auto \
 \
     --output eDP-1-1 \
     --dpi 196 \
     --pos 0x0 \
-    --scale 0.5x0-.5 \
+    --scale 0.5x0.5 \
     --mode 2560x1600
 
 # kill apps with wrong scaling
