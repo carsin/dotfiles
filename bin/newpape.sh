998ca4a7f7976749958457d2d9a2969712c2e9cb
carsin <20868514+carsin@users.noreply.github.com>
2022-01-15 19:25:17 -0800

    add missing *facepalm*

--- bin/newpape.sh
+++ bin/newpape.sh
@@ -1,2 +1,5 @@
-wal -a 85 --backend haishoku -i ~/files/photos/wallpapers/wal &
-killall -USR1 st 
+echo "running wal..."
+timeout 3 wal -a 85 --backend haishoku -i ~/files/photos/wallpapers/wal & 
+echo "done! sending refresh signal to st..."
+killall -USR1 /usr/local/bin/st 
+echo "complete"
