b89e3e1fc2a95a5f15f3f6b6fcaa98591b7c3038
carsin <20868514+carsin@users.noreply.github.com>
2022-01-18 16:48:15 -0800

    zathura + pywal

--- bin/newpape.sh
+++ bin/newpape.sh
@@ -2,3 +2,4 @@ timeout 2 wal -a 85 --backend haishoku -i ~/files/photos/wallpapers/ultrawide &
 # sleep 1 && killall -USR1 /usr/local/bin/st &
 wait
 pywalfox update & # may be causing instability
+zathura-pywal -a 0.85
