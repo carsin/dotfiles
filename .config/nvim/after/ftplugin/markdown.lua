a7745be1de449d24663fe25e0ff69c15c36813c8
carsin <20868514+carsin@users.noreply.github.com>
2022-01-24 15:48:26 -0800

    nvim improve md & add missing

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- remove heavy unneeded sources from markdown 
+local cmp = require('cmp')
+local sources = cmp.get_config().sources
+for i = #sources, 1, -1 do
+  if sources[i].name == 'buffer' or 'LuaSnip' or 'tmux' then
+    table.remove(sources, i)
+  end
+end
+cmp.setup.buffer({ sources = sources })
