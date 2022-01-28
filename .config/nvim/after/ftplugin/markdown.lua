d2e16b0a1e237ec97424409d7b1f0e1c2954fb93
carsin <20868514+carsin@users.noreply.github.com>
2022-01-28 13:14:54 -0800

    cleanup

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ local cmp = require('cmp')
 local sources = cmp.get_config().sources
 for i = #sources, 1, -1 do
   local c = sources[i].name
-  if c == 'buffer' or c == 'tmux' or c == 'luasnip' then
+  if c == 'buffer' or c == 'tmux' or c == 'luasnip' or c == 'path' then
     table.remove(sources, i)
   end
 end
