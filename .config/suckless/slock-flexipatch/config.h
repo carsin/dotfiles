0ef22f81ecadd1a4a012e4fae6c247786fcdf9f2
carsin <20868514+carsin@users.noreply.github.com>
2022-02-05 16:06:32 -0800

    add missing

---
+++ .config/suckless/slock-flexipatch/config.h
@@ -0,0 +1,154 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody"; // use "nobody" for arch
+
+static const char *colorname[NUMCOLS] = {
+	#if DWM_LOGO_PATCH && !BLUR_PIXELATED_SCREEN_PATCH
+	[BACKGROUND] =  "#282828", /* after initialization */
+	#endif // DWM_LOGO_PATCH
+	[INIT] =   "#458588",     /* after initialization */
+	[INPUT] =  "#8ec07c",   /* during input */
+	[FAILED] = "#fb4934",   /* wrong password */
+	#if CAPSCOLOR_PATCH
+	[CAPS] =   "#d65d0e",       /* CapsLock on */
+	#endif // CAPSCOLOR_PATCH
+	#if PAMAUTH_PATCH
+	[PAM] =    "#9400D3",   /* waiting for PAM */
+	#endif // PAMAUTH_PATCH
+	#if KEYPRESS_FEEDBACK_PATCH
+	[BLOCKS] = "#ebdbb2",   /* key feedback block */
+	#endif // KEYPRESS_FEEDBACK_PATCH
+};
+
+#if MESSAGE_PATCH || COLOR_MESSAGE_PATCH
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ebdbb2";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso10646-1";
+#endif // MESSAGE_PATCH | COLOR_MESSAGE_PATCH
+
+#if DWM_LOGO_PATCH
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;   /* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[] = {
+   /* x    y   w   h */
+   { 0,    3,  1,  3 },
+   { 1,    3,  2,  1 },
+   { 0,    5,  8,  1 },
+   { 3,    0,  1,  5 },
+   { 5,    3,  1,  2 },
+   { 7,    3,  1,  2 },
+   { 8,    3,  4,  1 },
+   { 9,    4,  1,  2 },
+   { 11,   4,  1,  2 },
+};
+#endif // DWM_LOGO_PATCH
+
+#if XRESOURCES_PATCH
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		#if DWM_LOGO_PATCH && !BLUR_PIXELATED_SCREEN_PATCH
+		{ "color2",       STRING,  &colorname[BACKGROUND] },
+		#endif //DWM_LOGO_PATCH
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		#if CAPSCOLOR_PATCH
+		{ "color3",       STRING,  &colorname[CAPS] },
+		#endif // CAPSCOLOR_PATCH
+		#if PAMAUTH_PATCH
+		{ "color5",       STRING,  &colorname[PAM] },
+		#endif // PAMAUTH_PATCH
+		#if MESSAGE_PATCH || COLOR_MESSAGE_PATCH
+		{ "color6",       STRING,  &text_color },
+		#endif // MESSAGE_PATCH | COLOR_MESSAGE_PATCH
+};
+#endif // XRESOURCES_PATCH
+
+#if ALPHA_PATCH
+/* lock screen opacity */
+static const float alpha = 1;
+#endif // ALPHA_PATCH
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+#if AUTO_TIMEOUT_PATCH
+/* length of time (seconds) until */
+static const int timeoffset = 60;
+
+/* should [command] be run only once? */
+static const int runonce = 0;
+
+/* command to be run after [time] has passed */
+static const char *command = "doas poweroff";
+#endif // AUTO_TIMEOUT_PATCH
+
+#if FAILURE_COMMAND_PATCH
+/* number of failed password attempts until failcommand is executed.
+   Set to 0 to disable */
+static const int failcount = 0;
+
+/* command to be executed after [failcount] failed password attempts */
+static const char *failcommand = "shutdown";
+#endif // FAILURE_COMMAND_PATCH
+
+#if SECRET_PASSWORD_PATCH
+static const secretpass scom[] = {
+	/* Password             command */
+	{ "shutdown",           "doas poweroff"},
+};
+#endif // SECRET_PASSWORD_PATCH
+
+#if BLUR_PIXELATED_SCREEN_PATCH
+/* Enable blur */
+#define BLUR
+/* Set blur radius */
+static const int blurRadius = 9;
+/* Enable Pixelation */
+//#define PIXELATION
+/* Set pixelation radius */
+static const int pixelSize = 50;
+#endif // BLUR_PIXELATED_SCREEN_PATCH
+
+#if CONTROLCLEAR_PATCH
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 1;
+#endif // CONTROLCLEAR_PATCH
+
+#if DPMS_PATCH
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 60;
+#endif // DPMS_PATCH
+
+#if KEYPRESS_FEEDBACK_PATCH
+static short int blocks_enabled = 1; // 0 = don't show blocks
+static const int blocks_width = 0; // 0 = full width
+static const int blocks_height = 16;
+
+// position
+static const int blocks_x = 0;
+static const int blocks_y = 1630;
+
+// Number of blocks
+static const int blocks_count = 10;
+#endif // KEYPRESS_FEEDBACK_PATCH
+
+#if PAMAUTH_PATCH
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
+#endif // PAMAUTH_PATCH
+
+#if QUICKCANCEL_PATCH
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
+#endif // QUICKCANCEL_PATCH
