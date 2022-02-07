717771204b4b3b62352b6b3ab5ab25d0113b1121
carsin <20868514+carsin@users.noreply.github.com>
2022-02-07 11:07:58 -0800

    trying faster logstart

--- .config/nvim/lua/settings/zk.lua
+++ .config/nvim/lua/settings/zk.lua
@@ -1,5 +1,23 @@
-local lspc = require'settings.lsp.lspconfig'.get_config()
+local lspc = require("settings.lsp.lspconfig").get_config()
+local zk = require("zk")
+local commands = require("zk.commands")
 local M = {}
+
+-- add :ZkOrphans to list unreferenced files
+commands.add("ZkOrphans", function(options)
+	options = vim.tbl_extend("force", { orphan = true }, options or {})
+	zk.edit(options, { title = "Zk Orphans" })
+end)
+
+-- add :ZkRecents
+local function make_edit_fn(defaults, picker_options)
+	return function(options)
+		options = vim.tbl_extend("force", defaults, options or {})
+		zk.edit(options, picker_options)
+	end
+end
+commands.add("ZkOrphans", make_edit_fn({ orphan = true }, { title = "Zk Orphans" }))
+commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
 
 require("zk").setup({
   picker = "telescope",
@@ -47,3 +65,4 @@ M.set_mappings = function()
 end
 
 return M
+
