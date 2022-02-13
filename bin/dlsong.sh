ddbffd3bc74b03d29952fbe9ce0de35b271b3f0a
carsin <20868514+carsin@users.noreply.github.com>
2022-02-13 12:00:13 -0800

    scripts souncloud supp & better formats

---
+++ bin/dlsong.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /home/carson/files/music/spotify/bp/scrape2/ || exit
+
+if [[ $1 = "" ]]; then
+    read -p "url: " url 
+else
+    url=$1
+fi
+
+youtube-dl --no-continue --download-archive downloaded.txt --extract-audio -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata "$url"
---
+++ bin/updateplaylist.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+# shellcheck disable=2016
+
+cd ~/files/music/spotify/bp/scrape2/
+
+# CSX
+echo 'Downloading CSX'
+
+youtube-dl --download-archive downloaded.txt --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL4PofEZT9GHow6tHzD8Gp6NpSXbPHO97B --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+
+
+echo 'Playlist CSX finished downloading'
+
+# Fear 1
+echo 'Downloading Fear 1'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3e4VOWWi0V_WsnC9dORiJLL --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+
+echo 'Playlist Fear 1 finished downloading'
+
+# Fear 2
+echo 'Downloading Fear 2'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3dY1X6gsSmfgRK2yA8Pr2Al --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+
+echo 'Playlist Fear 2 finished downloading'
+
+# DNA
+echo 'Downloading DNA'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLrA41AIkBqnK0K3Nueo0JTH2P-pkEAZEz --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+
+echo 'Playlist DNA finished downloading'
+
+# Kloudz 333
+echo 'Downloading Kloudz 33'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLBTAY2snT7uHPM87CB0DLXbrjdY9CjbDf --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist Kloudz 333 finished downloading'
+
+# C$X Test
+echo 'Downloading MEX: C$X Test'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3dXQBHEs429z_F1iTHf2Oe4 --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist MEX: C$X Test finished downloading'
+
+# Mex test
+echo 'Downloading MEX Test'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL4PofEZT9GHrkU5YzY_EsDA_0A-qCxeYE --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist MEX Test finished downloading'
+
+# Luna to the moon
+echo 'Downloading Luna: to the moon'
+# --restrict-filenames fixes undownloadable ascii chars
+# youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - (uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLZL0uQ9nkwWxXdJMaoMntO_CYKjVJrEAE --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200 --restrict-filenames
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - (uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLZL0uQ9nkwWxXdJMaoMntO_CYKjVJrEAE --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200 --restrict-filenames
+echo 'Playlist Luna finished downloading'
+
+# Permaban
+echo 'Downloading Permaban'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLZW5bl8Qpvk0yx0w9cNoRUtdm8zKuZIqy --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata
+echo 'Playlist Permaban finished downloading'
+
+# Neverland
+echo 'Downloading Neverland'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLdIDJawYJSxWzXIJV1WmOvV20ZU5ye2LJ --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist Neverland finished downloading'
+
+# sextek
+# echo 'Downloading sextek'
+# youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLTwLDJl0Tns0W7HUtdBxFdOWnOXtwkw5l --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata
+# echo 'Playlist sextek finished downloading'
+
+# 3am
+echo 'Downloading 3am'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3eVg_mwZ3AA216qCJ_mU2w3 --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist 3am finished downloading'
+
+# Sky
+echo 'Downloading sky'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3dY1X6gsSmfgRK2yA8Pr2Al --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist sky finished downloading'
+
+# KING
+echo 'Downloading KING'
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PL_fNrCkNoX3dXQBHEs429z_F1iTHf2Oe4 --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist KING finished downloading'
+
+# firgi Tired
+echo "Downloading firgi's tired"
+youtube-dl --download-archive downloaded.txt -f m4a --extract-audio --audio-format best -S'+acodec:140,m4a,mp3' -o '%(title)s - (uploader)s.%(ext)s' https://www.youtube.com/playlist?list=PLdIDJawYJSxVyTuflOiKIxVfczb6h5SFA --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata --trim-filenames 200
+echo 'Playlist tired finished downloading'
+
+echo 'EDM update complete!'
