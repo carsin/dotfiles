8403067461006b1f35008fb350e90b925fca57e8
carsin <20868514+carsin@users.noreply.github.com>
2022-02-16 12:27:16 -0800

    speed up pkg compilation

--- bin/dlsong.sh
+++ bin/dlsong.sh
@@ -8,4 +8,4 @@ else
     url=$1
 fi
 
-youtube-dl --no-continue --download-archive downloaded.txt --extract-audio -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata "$url"
+youtube-dl --no-continue --download-archive downloaded.txt --audio-format best --extract-audio -S'+acodec:140,m4a,mp3' -o '%(title)s - %(uploader)s.%(ext)s' --ignore-errors --add-metadata --embed-thumbnail --compat-options embed-metadata "$url"
