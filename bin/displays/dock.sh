0a76df1ef6f8a7e65ef52746add42040c02c7da4
carsin <20868514+carsin@users.noreply.github.com>
2022-03-02 10:59:56 -0800

    use tmux.nvim & better dock layout

--- bin/displays/dock.sh
+++ bin/displays/dock.sh
@@ -5,7 +5,7 @@ newdpi=109
 
 echo "Xft.dpi: $newdpi" | xrdb -merge
 
-xrandr --output DP-1 --off --output DP-2 --mode 3440x1440 --pos 2560x160 --rotate normal --dpi $newdpi  \
---output DP-4 --primary --mode 2560x1440 --pos 3000x1600 --rotate normal --dpi $newdpi --rate 165.00 \
---output eDP-1-1 --mode 2560x1600 --pos 0x0 --rotate normal 
+xrandr --output DP-1 --off --output DP-2 --mode 3440x1440 --pos 2120x0 --rotate normal --dpi $newdpi  \
+--output DP-4 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal --dpi $newdpi --rate 165.00 \
+--output eDP-1-1 --mode 2560x1600 --pos 0x1440 --rotate normal 
 
--- bin/displays/ultrawide.sh
+++ bin/displays/ultrawide.sh
@@ -6,7 +6,11 @@ newdpi=109
 # set new xresources dpi
 echo "Xft.dpi: $newdpi" | xrdb -merge
 
+   --output DP-5 --off --output eDP-1-1 --mode 2560x1600 --pos 0x1440 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+
 xrandr \
+    --output DP-1 --off --output DP-2 --mode 3440x1440 --pos 2120x0 --rotate normal \
+    --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal
     --output DP-4 \
     --primary \
     --dpi $newdpi \
