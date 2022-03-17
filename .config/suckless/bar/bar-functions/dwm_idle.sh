9aa3fd075bc10389399e272e927125f9c8acd4af
carsin <20868514+carsin@users.noreply.github.com>
2022-03-16 19:08:28 -0700

    clnea

--- .config/suckless/bar/bar-functions/dwm_idle.sh
+++ .config/suckless/bar/bar-functions/dwm_idle.sh
@@ -16,3 +16,39 @@ dwm_idle () {
 }
 
 dwm_idle
+
+# TODO: replace xprintidle with this custom async functionality since doing it
+# in main loop wouldn't work'
+idleloop() {
+    touch /tmp/.{,last_}input
+    cmd='stat --printf="%s"'
+    idletime=120
+    a=2
+    t=0
+    while true
+    do
+        timeout 1 xinput test-xi2 --root > /tmp/.input
+        
+        if [[ `eval "$cmd" /tmp/.input` == `eval "$cmd" /tmp/.last_input` ]]
+        then
+            let t++ # increases $t by 1
+        else
+            t=0     # resets $t
+        fi
+
+        mv /tmp/.{,last_}input -f
+
+        if [ "$t" -ge "$idletime" ] && [[ $a == "2" ]]
+        then
+            echo "user has gone idle"
+            a=1
+        fi
+        if [ "$t" -lt "$idletime" ] && [[ $a == "1" ]]
+        then
+            echo "user has come back from idle"
+            a=2
+        fi
+    done
+}
+
+# idleloop &
