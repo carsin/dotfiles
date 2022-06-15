43cb6a86eb1234ebf40c67c3bb16573d632b1bbf
carsin <20868514+carsin@users.noreply.github.com>
2022-06-14 21:11:10 -1000

    fix displays

--- bin/displays/dual2.sh
+++ bin/displays/dual2.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 2560x1600 --pos 0x544 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DP5 --mode 1920x1080 --pos 4480x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
+
+newdpi=96
+
+# set new xresources dpi
+echo "Xft.dpi: $newdpi" | xrdb -merge
+
+xrandr --output eDP1 \
+    --mode 2560x1600 \
+    --pos 0x544 \
+    --rotate normal \
+    --output DP1 --off --output DP2 --off --output DP3 --off \
+    --output DP4 --primary --dpi $newdpi --rate 144.00 --mode 1920x1080 --pos 2560x0 --rotate normal \
+    --output DP5 --dpi $newdpi --rate 144.00 --mode 1920x1080 --pos 4480x0 --rotate normal \
+    --output HDMI1 --off --output VIRTUAL1 --off
