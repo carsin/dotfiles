fc9e49f7c361e5fc7c156032f7a6380ce5e08df7
carsin <20868514+carsin@users.noreply.github.com>
2022-09-06 16:12:16 -1000

    fixes

--- .config/suckless/bar/bar-functions/dwm_resources.sh
+++ .config/suckless/bar/bar-functions/dwm_resources.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-dwm_battery () {
-    CHARGE=$(acpi | awk '{ print $4 }' | tr -d \,)
-    STATUS=$(cat /sys/class/power_supply/BAT0/status)
-
-    if [[ "$STATUS" != "Full" ]]; then
-        # printf "%s" "$SEP1"
-        if [ "$IDENTIFIER" = "unicode" ]; then
-            if [ "$STATUS" = "Charging" ]; then
-                printf " %s" "$CHARGE" 
-            else
-                printf " %s" "$CHARGE" 
-            fi
-        fi
-        # printf "%s" "$SEP2 "
-    fi
-}
-
 dwm_resources() {
     # get all the infos first to avoid high resources usage
     free_output=$(free -h | grep Mem)
@@ -26,6 +9,8 @@ dwm_resources() {
     # CPU usage
     CPU=$(top -bn1 | grep Cpu | awk '{print $2}')%
     TEMP=$(sensors | grep -oP 'Package.*?\+\K[0-9.]+')
+    BATSTATUS=$(cat /sys/class/power_supply/BAT0/status)
+    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
     if [ "$TEMP" = "" ]; then
         TEMP=$(sensors | grep -oP 'Physical.*?\+\K[0-9.]+')
     fi
@@ -33,8 +18,16 @@ dwm_resources() {
 
     printf "%s" "$SEP1"
     # ﬙
-    printf "%s° %s %s " "$(echo "$TEMP" | cut -f1 -d'.')" "$CPU" "$MEMUSED"
-    dwm_battery
+    printf "%s %s° %s" "$CPU" "$(echo "$TEMP" | cut -f1 -d'.')" "$MEMUSED"
+    if [[ "$CHARGE" != "100" ]]; then
+        if [ "$IDENTIFIER" = "unicode" ]; then
+            if [ "$BATSTATUS" = "Charging" ]; then
+                printf " %s%%" "$CHARGE" 
+            else 
+                printf " %s%%" "$CHARGE" 
+            fi
+        fi
+    fi
     printf "%s" "$SEP2"
 }
 
