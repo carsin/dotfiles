be7b2177eb3c743759ecfc0e70025bd81c0f6689
carsin <20868514+carsin@users.noreply.github.com>
2022-10-31 14:15:20 -0700

    broken b tweaks & qol changes

--- .local/bin/displays/dock.sh
+++ .local/bin/displays/dock.sh
@@ -1,11 +1,20 @@
-#!/usr/bin/sh
+#!/bin/bash
+# enable laptop gpu if disabled
+MODE=$(supergfxctl -g)
+if [[ "$MODE" == "integrated" ]]; then
+    supergfxctl -m hybrid && exit
+fi
 
-export LIBVA_DRIVER_NAME=nvidia
+asusctl profile -P balanced # enter higher power mode
+
+# set proper dpi for monitors
 newdpi=109
-
 echo "Xft.dpi: $newdpi" | xrdb -merge
 
-xrandr --output DP-1 --off --output DP-0 --mode 3440x1440 --pos 2120x0 --rotate normal --dpi $newdpi  \
---output DP-4 --primary --mode 2560x1440 --pos 2560x1440 --rotate normal --dpi $newdpi --rate 165.00 \
---output eDP-1-1 --mode 2560x1600 --pos 0x1440 --rotate normal 
+# setup monitor layout
+xrandr --output eDP-1 --mode 2560x1600 --pos 0x1440 --dpi $newdpi --rate 165.00 --rotate normal --output DP-1-1 --mode 3440x1440 --dpi $newdpi --rate 60.00 --pos 2120x0 --rotate normal --output DP-2 --off --output HDMI-1-0 --mode 2560x1440 --pos 2560x1440 --primary --rate 165.00 --rotate normal --output DP-1-0 --off --output DP-1-2 --off
 
+xset s 3600 3600 # blank screen after 1 hour
+
+# fix wallpaper
+exec ~/.local/bin/newpape.sh
